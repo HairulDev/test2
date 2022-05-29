55199c2449c8a5e83ec4cb5ac9811a79a6db5515
Hairul dev <khairullah.aplikasi@gmail.com>
2022-05-29 10:54:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-import PostDetails from './components/PostDetails/PostDetails';
+import User from './components/User/User';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
-import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
 
 import './assets/bootstrap/css/bootstrap.min.css';
 
@@ -17,12 +16,10 @@ const App = () => {
       <div className='container'>
           <Navbar />
           <Switch>
-            <Route path="/" exact component={() => <Redirect to="/posts" />} />
-            <Route path="/posts" exact component={Home} />
-            <Route path="/posts/search" exact component={Home} />
-            <Route path="/posts/:id" exact component={PostDetails} />
-            <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
-            <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+            <Route path="/" exact component={() => <Redirect to="/users" />} />
+            <Route path="/users" exact component={Home} />
+            <Route path="/users/:id" exact component={User} />
+            <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/users" />)} />
           </Switch>
       </div>
     </BrowserRouter>
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-const Home = () => {
+const Post = () => {
   const { posts, isLoading } = useSelector((state) => state.posts);
 
   const user = JSON.parse(localStorage.getItem('profile'));
@@ -11,7 +11,6 @@ const Home = () => {
   if (!posts.length && !isLoading) return 'No posts';
 
   return (
-
       <div className="col-md-12 mb-2">
         <div className="card">
           <div className="card-body text-center">
@@ -20,7 +19,7 @@ const Home = () => {
             <h5 className="card-title mb-3">{user?.result.name}</h5>
             <h6 className="card-muted">{user?.result.email}</h6>
             {(user?.result?.googleId === user?.creator || user?.result?._id === user?.creator) && (
-              <button className="btn btn-outline-info btn-sm"  onClick={() => history.push(`/users/${user?.result?._id}`)}>Edit</button>
+              <button className="btn btn-outline-info btn-sm"  onClick={() => history.push(`/posts/${user?.result?._id}`)}>Edit</button>
             )}
           </div>
         </div>
@@ -28,4 +27,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Post;
